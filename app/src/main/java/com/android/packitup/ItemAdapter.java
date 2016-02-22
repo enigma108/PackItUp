194a6e3b185cebdd8af182cb46b9cc67cefa190e
enigma108 <vivekphone1@gmail.com>
2016-02-22 20:24:18 +0530

    recyclerview added

---
+++ app/src/main/java/com/android/packitup/ItemAdapter.java
@@ -0,0 +1,99 @@
+package com.android.packitup;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>{
+
+    private static final boolean DUMMY_DATA = true;
+
+    private List<Item> mDataset;
+
+    public ItemAdapter() {
+
+        mDataset = new ArrayList<Item>();
+        if(DUMMY_DATA) {
+            addDummyData();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_row, parent, false);
+        ViewHolder vh = new ViewHolder(v, parent.getContext(), this);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        final Item item = mDataset.get(position);
+        holder.mTextView.setText(item.getItemName());
+
+        if(item.isSelected()) {
+            holder.mAppCompatCheckBox.setChecked(true);
+        } else {
+            holder.mAppCompatCheckBox.setChecked(false);
+        }
+
+        holder.mAppCompatCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                item.setIsSelected(isChecked);
+            }
+        });
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        // each data item is just a string in this case
+        private TextView mTextView;
+        private AppCompatCheckBox mAppCompatCheckBox;
+        private ItemAdapter parent;
+        private boolean checked = false;
+
+        public ViewHolder(View v, Context c, ItemAdapter parent) {
+            super(v);
+
+            mTextView = (TextView) v.findViewById(R.id.item_text);
+            mAppCompatCheckBox = (AppCompatCheckBox)v.findViewById(R.id.checkbox);
+            View viewStyle = v.findViewById(R.id.view_style);
+            int[] attrs = new int[]{android.support.v7.appcompat.R.attr.selectableItemBackground};
+            TypedArray ta = c.obtainStyledAttributes(attrs);
+            int resid = ta.getResourceId(0, 0);
+            ta.recycle();
+            viewStyle.setBackgroundResource(resid);
+            this.parent = parent;
+        }
+    }
+
+    private void addDummyData() {
+        mDataset.add(new Item("Coffee", true));
+        mDataset.add(new Item("Banana"));
+        mDataset.add(new Item("Custard Apple"));
+        mDataset.add(new Item("Dates"));
+        mDataset.add(new Item("Grape"));
+        mDataset.add(new Item("Jack Fruit"));
+        mDataset.add(new Item("Marshmallow"));
+        mDataset.add(new Item("JellyBean"));
+        mDataset.add(new Item("Kitkat"));
+        mDataset.add(new Item("ButterCup"));
+    }
+}
--- app/src/main/java/com/android/packitup/ItemsFragment.java
+++ app/src/main/java/com/android/packitup/ItemsFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,9 +15,15 @@ public class ItemsFragment extends Fragment {
 
     private static final String TAG = ItemsFragment.class.getSimpleName();
 
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+    private ItemAdapter mItemAdapter;
+    private Context context;
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        this.context = context;
     }
 
     @Override
@@ -29,6 +37,21 @@ public class ItemsFragment extends Fragment {
 
         Log.d(TAG, "onCreateView");
         View view = inflater.inflate(R.layout.items_main, container, false);
+
+        //init recyclerview
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+
+        //all rows are equal size -- perf benefit
+        mRecyclerView.setHasFixedSize(true);
+
+        //init LinearLayoutManager
+        mLayoutManager = new LinearLayoutManager(context);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //init an adapter
+        mItemAdapter = new ItemAdapter();
+        mRecyclerView.setAdapter(mItemAdapter);
+
         return view;
     }
 }
