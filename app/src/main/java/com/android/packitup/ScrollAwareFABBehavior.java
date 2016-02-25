59014e19f2fc51fcb837f9a513a2da19bda06840
enigma108 <vivekphone1@gmail.com>
2016-02-25 16:29:08 +0530

    added FAB behavior

---
+++ app/src/main/java/com/android/packitup/ScrollAwareFABBehavior.java
@@ -0,0 +1,37 @@
+package com.android.packitup;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Hides the FAB when user scrolls down.
+ * This behavior handles the FAB from obstructing the checkboxes in RecyclerView.
+ */
+public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
+
+    public ScrollAwareFABBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL||super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+
+        if(dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.hide();
+        }
+        else if(dyConsumed < 0 && child.getVisibility() != View.VISIBLE){
+            child.show();
+
+        }
+    }
+}
